d8a5315d1fcf84d5b0786b7dcaf726d16cd92925
Ryan London <ryel590@gmail.com>
2019-01-21 23:28:01 -0800

    saving all values to local storage, some styling

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 const userInputs = {
+	enabled: false,
 	headers: [],
 	domains: [],
 }
@@ -8,17 +9,21 @@ chrome.runtime.onInstalled.addListener(function() {
 })
 
 chrome.runtime.onMessage.addListener((message) => {
-	const key = Object.keys(message)[0]
-	const arr = message[key].split('\n')
-	userInputs[key] = arr
-	console.log(userInputs)
+	const [key, value] = Object.entries(message)[0]
+	chrome.storage.local.set({ [key]: value })
+	if (typeof value === 'string') {
+		const lines = value.split('\n')
+		userInputs[key] = lines
+	} else {
+		userInputs[key] = value
+	}
 })
 
 // TODO: make this work
-chrome.webRequest.onBeforeSendHeaders.addListener(
-	function(details) {
-		console.log('sending headers', details)
-	},
-	{ urls: userInputs.domains },
-	['blocking', 'requestHeaders']
-)
+// chrome.webRequest.onSendHeaders.addListener(
+// 	function(details) {
+// 		console.log('sending headers', details)
+// 	},
+// 	{ urls: ['<all_urls>'], types: ['xmlhttprequest'] },
+// 	['requestHeaders']
+// )
--- popup.js
+++ popup.js
@@ -1,9 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const inputs = Array.from(document.getElementsByClassName('input'))
-	inputs.forEach((input) => input.addEventListener('keyup', handleInputChange))
+	const textFields = ['domains', 'headers']
+	const checkbox = 'enabled'
+
+	const enabled = document.getElementById(checkbox)
+	enabled.addEventListener('click', handleCheckboxChange)
+	loadSavedCheckboxValue(checkbox)
+
+	textFields.forEach((fieldName) => {
+		document.getElementById(fieldName).addEventListener('keyup', handleTextFieldChange)
+		loadSavedTextFieldValue(fieldName)
+	})
 })
 
-function handleInputChange(e) {
-	// TODO: maybe persist inputs in localstorage?
-	chrome.runtime.sendMessage({ [e.target.id]: e.target.value })
+function handleTextFieldChange(e) {
+	const key = e.target.id
+	chrome.runtime.sendMessage({ [key]: e.target.value })
+}
+
+function handleCheckboxChange(e) {
+	const key = e.target.id
+	const iconStatus = e.target.checked ? 'enabled' : 'disabled'
+	chrome.runtime.sendMessage({ [key]: e.target.checked })
+	chrome.browserAction.setIcon({
+		path: `icons/cors-extension-${iconStatus}-64.png`,
+	})
+}
+
+function loadSavedTextFieldValue(key) {
+	chrome.storage.local.get(key, (result) => {
+		document.getElementById(key).value = result[key]
+	})
+}
+
+function loadSavedCheckboxValue(key) {
+	chrome.storage.local.get(key, (result) => {
+		document.getElementById(key).checked = result[key]
+	})
 }
